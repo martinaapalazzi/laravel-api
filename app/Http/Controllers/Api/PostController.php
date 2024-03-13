a640818ead3be4e47d117745bf4216e9d1af1ed5
martinaapalazzi <martinaapalazzi@gmail.com>
2024-03-13 13:48:09 +0100

    defined index and show functions

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -10,5 +10,23 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::with('type', 'technologies')->paginate(5);
+
+        return response()->json([
+            'success' => true,
+            'results' => $posts,
+        ]);
+    }
+
+    public function show(string $slug)
+    {
+        $post = Post::where('slug', $slug)->firstOrFail();
+
+        return response()->json([
+            'success'=> true,
+            'results'=> $post,
+        ]);
+    }
 }
