27918e8c983952d9b4eabb71c262efe56e540ee5
martinaapalazzi <martinaapalazzi@gmail.com>
2024-03-15 17:14:07 +0100

    created model e migration

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -5,12 +5,12 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+// Requests
+use App\Http\Requests\Contact\StoreRequest as ContactStoreRequest;
+
 class ContactController extends Controller
 {
-    public function store(Request $request) {
-        return response()->json({
-            'success' => true,
-        }
-    );
+    public function store(ContactStoreRequest $request) {
+        return response()->json($request->validated());
     }
 }
